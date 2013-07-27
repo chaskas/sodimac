d8bedd6d8d3ca9e2233f66f47cebaaa12b2c4801
Rodrigo Campos <mr.chaskas@gmail.com>
2013-07-27 11:33:59 -0400

    creado rest para setRespuesta POST

--- apps/frontend/modules/api/actions/actions.class.php
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -28,4 +28,71 @@ class apiActions extends sfActions
       ->createQuery('a')
       ->execute();
   }
+
+  public function executeSetRespuesta(sfWebRequest $request)
+  {
+    $data     = $this->getPayload($request);
+    $format   = $request->getRequestFormat();
+    $response = $this->getResponse();
+
+    if (!empty($data))
+    {
+      $form = new EncuestaRespuestasForm();
+
+      // Disable this protection
+      $form->disableLocalCSRFProtection();
+      unset($form[$form->getCSRFFieldName()]);
+
+      $form->bind($data);
+
+      if ($form->isValid())
+      {
+        $form->save(); // Persist a new Pony
+        $response->setStatusCode(201); // 201 = Created
+        // It's a good practice that to add the new ressource location in headers
+        //$response->addHttpMeta('Location', $this->generateUrl('pony_show', array('slug' => $form->getObject()->slug, 'sf_format' => $format), true));
+      }
+      else
+      {
+        // var_dump($data);
+        // foreach($form['EncuestaCabeceraRespuestas'] as $key=>$field) {
+        //   echo $key."<br/>";
+        // }
+
+        $response->setStatusCode(406); // Invalid. We should provide debug info in the response body here.
+      }
+    }
+    else
+    {
+      $response->setStatusCode(400); // No payload, bad request
+    }
+
+    return sfView::NONE;
+  }
+
+  protected function getPayload($request)
+  {
+    $payload = $request->getContent();
+    $format  = $request->getRequestFormat();
+    $data    = '';
+    
+    if (!empty($payload))
+    {
+      if ($format == 'json')
+      {
+        $data = (array) json_decode($payload,true);
+      }
+      elseif ($format == 'xml')
+      {
+        $parser = Doctrine_Parser::getParser($format);
+        $data = $parser->prepareData( simplexml_load_string($payload) );
+      }
+      else
+      {
+        throw new sfException("This format of pony isn't supported yet.");
+      }
+    }
+
+    return $data;
+  }
 }
