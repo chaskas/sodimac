6adc964f9ca8aff1206b9336bd33b6e9a017268b
Rodrigo Campos <mr.chaskas@gmail.com>
2013-08-27 17:06:03 -0400

    Hecho #16

--- apps/frontend/modules/api/templates/getPaisesByAplicacionSuccess.json.php
+++ apps/frontend/modules/api/templates/getPaisesByAplicacionSuccess.json.php
@@ -2,9 +2,10 @@
 <?php $nb = count($paises); $i = 0; ?>
 <?php foreach ($paises as $pais): ++$i ?>
 	{
-		"id_pais": "<?php echo $pais->getIdPais(); ?>",
-		"desc_pais": "<?php echo $pais->getPais(); ?>"
-		"signo_moneda": "<?php echo $pais->getPais()->getSignoMoneda(); ?>",
+		"id_pais": "<?php echo utf8_decode($pais->getIdPais()) ?>",
+		"desc_pais": "<?php echo utf8_decode($pais->getPais()) ?>"
+		"sigla": "<?php echo utf8_decode($pais->getPais()->getSigla()) ?>",
+		"signo_moneda": "<?php echo utf8_decode($pais->getPais()->getSignoMoneda()) ?>",
 		"con_decimal": "<?php echo $pais->getPais()->getConDecimal() ? '1' : '0'; ?>"
 	}
 	<?php echo $nb == $i ? '' : ',' ?>
--- apps/frontend/modules/api/templates/getPaisesByAplicacionSuccess.xml.php
+++ apps/frontend/modules/api/templates/getPaisesByAplicacionSuccess.xml.php
@@ -4,6 +4,9 @@
   <pais>
 		<id><?php echo $pais->getIdPais() ?></id>
 		<desc_pais><?php echo $pais->getPais() ?></desc_pais>
+		<sigla><?php echo $pais->getPais()->getSigla() ?></sigla>
+		<signo_moneda><?php echo $pais->getPais()->getSignoMoneda() ?></signo_moneda>
+		<con_decimal><?php echo $pais->getPais()->getConDecimal() ?></con_decimal>
   </pais>
 <?php endforeach ?>
 </paises>
