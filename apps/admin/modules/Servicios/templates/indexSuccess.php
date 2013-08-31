94d8f2cae5377863059f23ab340d15cf270960cb
chaskas <rodrigo@webdevel.cl>
2013-08-31 13:58:43 -0400

    Servicios: agregado botones en index

--- apps/admin/modules/Servicios/templates/indexSuccess.php
+++ apps/admin/modules/Servicios/templates/indexSuccess.php
@@ -24,11 +24,16 @@
         <td class="center"><a href="<?php echo url_for('Servicios/edit?id_servicio_tienda='.$servicio->getIdServicioTienda()) ?>"><?php echo $servicio->getIdServicioTienda() ?></a></td>
         <td><?php echo $servicio->getDescServicio() ?></td>
         <td class="center"><?php echo image_tag($servicio->getEstadoImg()) ?></td>
-        <td>
-          <?php //link_to 'play', status_play_path(station.id), :class => 'btn btn-mini' ?>
-          <?php //link_to 'stop', status_stop_path(station.id), :class => 'btn btn-mini' ?>
-          <?php //link_to t('.edit', :default => t("helpers.links.edit")),edit_station_path(station), :class => 'btn btn-mini' ?>
-          <?php //link_to t('.destroy', :default => t("helpers.links.destroy")), station_path(station), :method => :delete, :data => { :confirm => t('.confirm', :default => t("helpers.links.confirm", :default => 'Are you sure?')) }, :class => 'btn btn-mini btn-danger' ?>
+        <td class="center">
+          <?php echo link_to(
+                        'Editar',
+                        'Servicios/edit?id_servicio_tienda='.$servicio->getIdServicioTienda(),
+                        array('class'=>'btn btn-mini')); ?>
+          <?php echo link_to(
+                        'Eliminar',
+                        'delete_servicio',
+                        array('id_servicio_tienda'=>$servicio->getIdServicioTienda()),
+                        array('method'=>'delete','confirm' => '¿Estás seguro?','class'=>'btn btn-danger btn-mini')); ?>
         </td>
       </tr>
     <?php endforeach; ?>
