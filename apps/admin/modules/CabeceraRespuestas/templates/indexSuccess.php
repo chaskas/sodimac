dd520ce362e30444862d468bc1a180f81d8ac4b8
chaskas <rodrigo@webdevel.cl>
2013-08-31 13:34:23 -0400

    CabeceraRespuestas: agregado botones para opciones index

--- apps/admin/modules/CabeceraRespuestas/templates/indexSuccess.php
+++ apps/admin/modules/CabeceraRespuestas/templates/indexSuccess.php
@@ -23,14 +23,19 @@
     <?php foreach ($cabeceras as $cabecera): ?>
       <tr>
         <td class="center"><a href="<?php echo url_for('CabeceraRespuestas/edit?id_enc_cab_resp='.$cabecera->getIdEncCabResp()) ?>"><?php echo $cabecera->getIdEncCabResp() ?></a></td>
-        <td><?php echo $cabecera->getNroBoleta() ?></td>
+        <td class="center"><?php echo $cabecera->getNroBoleta() ?></td>
         <td class="center"><?php echo $cabecera->getFechaCompraFormatted() ?></td>
-        <td><?php echo $cabecera->getIdTienda() ?></td>
-        <td>
-          <?php //link_to 'play', status_play_path(station.id), :class => 'btn btn-mini' ?>
-          <?php //link_to 'stop', status_stop_path(station.id), :class => 'btn btn-mini' ?>
-          <?php //link_to t('.edit', :default => t("helpers.links.edit")),edit_station_path(station), :class => 'btn btn-mini' ?>
-          <?php //link_to t('.destroy', :default => t("helpers.links.destroy")), station_path(station), :method => :delete, :data => { :confirm => t('.confirm', :default => t("helpers.links.confirm", :default => 'Are you sure?')) }, :class => 'btn btn-mini btn-danger' ?>
+        <td class="center"><?php echo $cabecera->getIdTienda() ?></td>
+        <td class="center">
+          <?php echo link_to(
+                        'Editar',
+                        'CabeceraRespuestas/edit?id_enc_cab_resp='.$cabecera->getIdEncCabResp(),
+                        array('class'=>'btn btn-mini')); ?>
+          <?php echo link_to(
+                        'Eliminar',
+                        'delete_cabecera_respuestas',
+                        array('id_enc_cab_resp'=>$cabecera->getIdEncCabResp()),
+                        array('method'=>'delete','confirm' => '¿Estás seguro?','class'=>'btn btn-danger btn-mini')); ?>
         </td>
       </tr>
     <?php endforeach; ?>
