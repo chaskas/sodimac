1679a6d8d98896221195b87c306cfc82e85e808c
chaskas <rodrigo@webdevel.cl>
2013-10-14 18:30:48 -0300

    corregido error json

--- apps/frontend/modules/api/templates/getPaisesByAplicacionSuccess.json.php
+++ apps/frontend/modules/api/templates/getPaisesByAplicacionSuccess.json.php
@@ -3,7 +3,7 @@
 <?php foreach ($paises as $pais): ++$i ?>
 	{
 		"id_pais": "<?php echo utf8_decode($pais->getIdPais()) ?>",
-		"desc_pais": "<?php echo utf8_decode($pais->getPais()) ?>"
+		"desc_pais": "<?php echo utf8_decode($pais->getPais()) ?>",
 		"sigla": "<?php echo utf8_decode($pais->getPais()->getSigla()) ?>",
 		"signo_moneda": "<?php echo utf8_decode($pais->getPais()->getSignoMoneda()) ?>",
 		"con_decimal": "<?php echo $pais->getPais()->getConDecimal() ? '1' : '0'; ?>"
